2211b6822caf3658e159bf69ec41d5b7f950834a
viracsreng <srengvirac@hotmail.com>
2014-09-04 16:38:29 +0200

    add carousel

--- controllers/app.js
+++ controllers/app.js
@@ -1,6 +1,6 @@
 (function(){
 	var app = angular.module('gallery',[]);
-	
+		
 	app.controller('PanelController', function(){
 		this.tab = 0;
 		
@@ -13,7 +13,19 @@
 		}
 	});
 	
-	app.controller('ItemController',['$http','$scope',function($http, $scope){	    	
+	app.controller('ContainerController', function(){
+		this.tab = 0;
+		
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		}
+		
+		this.isSelected = function(checkTab){
+			return this.tab == checkTab;
+		}
+	});
+	
+	app.controller('ItemController',['$http','$scope', function($http, $scope){	    	
 		this.items = [];
 		
 		$scope.init = function(){
@@ -38,8 +50,33 @@
 							ctrl.items.push(item);						
 						}
 					}
-				}									
-			});		
-		}
+				}								
+			});	
+		};
+		
+		this.getItem = function(name){
+			var ctrl = this;
+			ctrl.items = [];
+			$http.get('../databases/index.json').success(function(data){
+				for(var index = 0; index < data.length; index++){
+					var item = data[index];
+					if(item.name == name){
+						ctrl.items.push(item);
+					}
+				}
+			});
+		}		
+	}]);
+	
+	app.controller('CarouselController',['$http','$scope', function($http, $scope){	    	
+		this.items = [];
+		
+		this.initCarousel = function(){
+		    var ctrl = this;
+			ctrl.items = [];
+			$http.get('../databases/index.json').success(function(data) {
+				ctrl.items = data;
+			});
+		};
 	}]);
 })();
