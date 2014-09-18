96a8c13e3406ba575f6d523163680aab2396a0eb
viracsreng <srengvirac@hotmail.com>
2014-09-18 15:12:17 +0200

    test sur doublon + slider

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-var express = require('express');
-var path = require('path');
+//modules
+var admzip = require('adm-zip');
+var app = express();
 var azure = require('azure');
+var bodyParser = require('body-parser');
+var express = require('express');
+var formidable = require('formidable');
 var fs = require('fs');
+var jf = require('jsonfile');
+var path = require('path');
 var url = require('url');
+var util = require('util');
+
 var storage_account = 'gallerie';
 var gallerieKey = 'SiQVY98VhO+NI1m6jfBMgB1M/00geM/puCgpMpRvsBSUz0H/xcgF77Wx9SiD7buJFvXZ9NTvyRNvf200CNT6Kg==';
 var package_container = 'packages';
 var index_container = 'descriptifs';
 var images_container = 'images';
-var admzip = require('adm-zip');
+
 var blobService = azure.createBlobService(storage_account,gallerieKey);
 var packages_folder = __dirname + '/databases/packages/';
-var bodyParser = require('body-parser');
-var jf = require('jsonfile');
-var util = require('util');
-var app = express();
-var formidable = require('formidable');
 
 app.use(express.static(__dirname + '/public'));
 app.use("/databases", express.static(__dirname + '/databases'));
@@ -26,7 +29,6 @@ app.use(bodyParser.json());
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 
-// app.use('/', routes);
 app.get('/gallery', function (req, res){
 	res.render('index.html');
 });
@@ -53,15 +55,17 @@ app.get('/product/download', function(req, res){
 			stream.on('end', function () {
 				res.end();
 			});			
-		}	
+		}
 	});			
 });
 
 app.get('/product/install', function(req, res){
     var item = url.parse(req.url).query;
-	res.json({ 
-		blobUrl : '/product/download?' + item
-	});
+    res.contentType('application/json');
+	var url = { 
+		"blobUrl" : '/product/download?' + item
+	};
+	res.end(url);
 });
 
 app.post('/addProduct', function(req, res){
@@ -74,9 +78,15 @@ app.post('/addProduct', function(req, res){
 	blobService.getBlobToFile(index_container, 'packages.json', __dirname + '/databases/packages.json', function(error, result, response){
 		var form = new formidable.IncomingForm();
 		form.parse(req, function(err, fields, files){
-				var name = fields.inputName.replace(' ', '');
+			var name = fields.inputName.replace(' ', '');
 			var product = {"type" : "product", "name" : fields.inputName, "link" : name, "description" : fields.inputDescription, "price" : 0, "file" : name, "package" : name, "category" : fields.inputCategory, "countView" : 0, "countOrder" : 0};
 			jf.readFile(__dirname + '/databases/packages.json', function (err, obj){
+				for(var i = 0; i < obj.length; i++){
+					if(obj[i]['link'] === name){
+						res.render('failure.html');
+					}
+				}
+					
 				obj.push(product);
 				jf.writeFile(__dirname + '/databases/packages.json', obj, function(err){
 					if(!err){
@@ -86,12 +96,59 @@ app.post('/addProduct', function(req, res){
 						console.log(err);
 					}
 				});
-				blobService.createBlockBlobFromFile(package_container, name + ".zip", files.inputPackage.path, function(error, res){});
-				blobService.createBlockBlobFromFile(images_container, name + ".jpg", files.inputImage.path, function(error, res){});
+				blobService.createBlockBlobFromFile(package_container, name + ".zip", files.inputPackage.path, function(){
+					blobService.createBlockBlobFromFile(images_container, name + ".jpg", files.inputImage.path, function(){
+						res.render('success.html');
+					});
+				});						
 			});
 		});
 	});
 });
+
+// app.get('/product/delete', function(req, res){
+// 	//delete package, image & json
+// 	var item = url.parse(req.url).query;
+// 	var filePath = packages_folder + item + '.zip';
+	
+// 	//package
+// 	blobService.deleteBlob(package_container, item + '.zip', function(error, response){
+// 		if(!error){
+// 			console.log("package de " + item + " supprimé");
+// 		}
+// 	});
+
+// 	//image
+// 	blobService.deleteBlob(images_container, item + '.jpg', function(error, response){
+// 		if(!error){
+// 			console.log("image de " + item + " supprimé");
+// 		}
+// 	});
+
+// 	//json
+// 	blobService.getBlobToFile(index_container, 'packages.json', __dirname + '/databases/packages.json', function(error, result, response){
+// 		jf.readFile(__dirname + '/databases/packages.json', function (err, obj){
+// 			for (var object in obj){
+// 				console.log(object['link']);
+// 				if(object['link'] === item){
+// 					console.log('delete ' + obj[object]);
+// 					delete obj[object];	
+// 				}
+// 			}
+
+// 			jf.writeFile(__dirname + '/databases/packages.json', obj, function(err){
+// 				if(!err){
+// 					console.log("nouvelle liste : " + obj);
+// 					res.render('success.html');
+// 					blobService.createBlockBlobFromFile(index_container, "packages.json", __dirname + '/databases/packages.json', function(){});
+// 				}
+// 				else{
+// 					console.log(err);
+// 				}
+// 			});	
+// 		});	
+// 	});	
+// });
 
 Init();
 var server = app.listen(3000, function() {
